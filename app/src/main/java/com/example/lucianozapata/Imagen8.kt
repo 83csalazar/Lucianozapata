6f3ad9c807b5d807b06ebcfa2120320bf8050cd4
83csalazar <135677850+83csalazar@users.noreply.github.com>
2023-11-27 22:08:54 -0300

    Initial commit

---
+++ app/src/main/java/com/example/lucianozapata/Imagen8.kt
@@ -0,0 +1,11 @@
+package com.example.lucianozapata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Imagen8 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imagen8)
+    }
+}
